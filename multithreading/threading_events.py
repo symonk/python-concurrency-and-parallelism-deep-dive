3e83dcbbc3708ef02eee492b7365237a3f36035d
symonk <jackofspaces@gmail.com>
2020-02-03 20:43:32 +0000

    starting on events

---
+++ multithreading/threading_events.py
@@ -0,0 +1,8 @@
+
+"""
+Threading events are a very simple concept; one thread emits an event while other threads are waiting to see such event
+In a nutshell, Events are used for inter-thread communication
+"""
+
+
+
